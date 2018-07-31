537bb33606d7eaa553a59c915ca1129d15a2c1a9
kmsheng <kmsh3ng@gmail.com>
2018-07-31 21:57:43 +0800

    feat: add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { PageHelloWorldComponent } from './page-hello-world/page-hello-world.component';
+import { PageTodosComponent } from './page-todos/page-todos.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'helloworld', pathMatch: 'full' },
+  { path: 'helloworld', component: PageHelloWorldComponent },
+  { path: 'todos', component: PageTodosComponent }
+];
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
