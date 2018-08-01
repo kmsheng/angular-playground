66f971c4f6dcb67c15f4f56d316f4aa5e939322c
kmsheng <kmsh3ng@gmail.com>
2018-08-02 00:06:41 +0800

    feat: add effects folder

---
+++ src/app/effects/counter.effects.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Action } from '@ngrx/store';
+import { Actions, Effect, ofType } from '@ngrx/effects';
+import { Observable, of } from 'rxjs';
+import { filter, catchError, map, mergeMap, switchMap, tap } from 'rxjs/operators';
+import { CounterState } from './../types';
+import { Store } from '@ngrx/store';
+
+@Injectable()
+export class CounterEffects {
+
+  // Listen for the 'INCREMENT' action
+  @Effect()
+  login$: Observable<Action> = this.actions$.pipe(
+    ofType('INCREMENT'),
+    tap(action => {
+      console.log('this.state$', this.state$);
+    }),
+    filter(() => false)
+  );
+
+  constructor(private actions$: Actions, private state$: Store<CounterState>) {}
+}
