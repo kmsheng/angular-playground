18f772dbcc37d2506a5cc0f58c582639adf6d288
kmsheng <kmsh3ng@gmail.com>
2018-08-01 21:13:18 +0800

    feat: add route counter

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PageHelloWorldComponent } from './page-hello-world/page-hello-world.component';
 import { PageTodosComponent } from './page-todos/page-todos.component';
+import { PageCounterComponent } from './page-counter/page-counter.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'helloworld', pathMatch: 'full' },
   { path: 'helloworld', component: PageHelloWorldComponent },
-  { path: 'todos', component: PageTodosComponent }
+  { path: 'todos', component: PageTodosComponent },
+  { path: 'counter', component: PageCounterComponent },
 ];
 
 @NgModule({
