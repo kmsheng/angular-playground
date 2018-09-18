262e92e0f787852e9105236229dfe4c117741159
kmsheng <kmsh3ng@gmail.com>
2018-09-18 09:02:34 +0800

    add secret route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { PageHelloWorldComponent } from './page-hello-world/page-hello-world.component';
 import { PageTodosComponent } from './page-todos/page-todos.component';
 import { PageCounterComponent } from './page-counter/page-counter.component';
+import { PageSecretComponent } from './page-secret/page-secret.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'counter', pathMatch: 'full' },
   { path: 'helloworld', component: PageHelloWorldComponent },
   { path: 'todos', component: PageTodosComponent },
   { path: 'counter', component: PageCounterComponent },
+  { path: 'secret', component: PageSecretComponent }
 ];
 
 @NgModule({
