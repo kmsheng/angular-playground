03c806257c8d78190c3f7a01927b7747d66587a8
kmsheng <kmsh3ng@gmail.com>
2018-09-19 16:52:05 +0800

    add search example using rxjs

--- src/app/page-rxjs/page-rxjs.component.ts
+++ src/app/page-rxjs/page-rxjs.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable, of } from 'rxjs';
-import { filter, catchError, map, mergeMap, switchMap,
-  tap, delay, exhaustMap, concatMap } from 'rxjs/operators';
+import { ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Observable, of, interval, merge, fromEvent, timer, interval } from 'rxjs';
+import { filter, catchError, map, mergeMap, switchMap, mapTo,
+  tap, delay, exhaustMap, concatMap, startWith, debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 
 const httpOptions = {
   headers: new HttpHeaders({'Content-Type': 'application/json'})
@@ -16,9 +17,30 @@ const httpOptions = {
 })
 export class PageRxjsComponent implements OnInit {
 
+  @ViewChild('input') input: ElementRef;
+
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
+    fromEvent(this.input.nativeElement, 'keyup')
+      .pipe(
+        map(event => event.target.value)
+        startWith(''),
+        debounceTime(400),
+        switchMap(id => {
+          return this.http.get(`/api/students/${id}`)
+            .pipe(
+              catchError(error => {
+                return of('empty inside');
+              })
+            );
+        })
+      )
+      .subscribe(
+        v => console.log('v', v),
+        err => console.log('err', err),
+        () => console.log('complete')
+      );
   }
 
 }
