4eedbc6d621326ac295eb43128351c36df5a4b1a
kmsheng <kmsh3ng@gmail.com>
2018-09-19 14:14:26 +0800

    feat: fill names to mocked data

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -5,13 +5,13 @@ export class InMemoryDataService implements InMemoryDbService {
   createDb() {
 
     const students = [
-      {id: 1, name: ''},
-      {id: 2, name: ''},
-      {id: 3, name: ''},
-      {id: 4, name: ''},
-      {id: 5, name: ''},
-      {id: 6, name: ''},
-      {id: 7, name: ''}
+      {id: 1, name: 'Alice'},
+      {id: 2, name: 'David'},
+      {id: 3, name: 'Carol'},
+      {id: 4, name: 'Trump'},
+      {id: 5, name: 'Joseph'},
+      {id: 6, name: 'JR'},
+      {id: 7, name: 'Johney'}
     ];
 
     return { students };
