3b103a73e95f52a26886b05fed68d18865d5bace
kmsheng <kmsh3ng@gmail.com>
2018-09-20 10:59:17 +0800

    feat: update counter.effects.ts

--- src/app/effects/counter.effects.ts
+++ src/app/effects/counter.effects.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import { Action } from '@ngrx/store';
 import { Actions, Effect, ofType } from '@ngrx/effects';
-import { Observable, of } from 'rxjs';
-import { filter, catchError, map, mergeMap, switchMap, tap, delay} from 'rxjs/operators';
+import { Observable, of, timer } from 'rxjs';
+import { filter, catchError, map, mergeMap, switchMap, concatMap, tap, delay, debounce} from 'rxjs/operators';
 import { Store } from '@ngrx/store';
+import { ActionDelayIncrement, ActionIncrement, CounterActionTypes.DELAY_INCREMENT } from '../reducers/counter';
 
 @Injectable()
 export class CounterEffects {
@@ -12,12 +13,11 @@ export class CounterEffects {
 
   // Listen for the 'INCREMENT' action
   @Effect()
-  count$: Observable<Action> = this.actions$.pipe(
-    ofType('DELAY_INCREMENT'),
-    tap(action => console.log('action', action)),
-    delay(1000),
-    map(action => {
-      return {type: 'INCREMENT'};
+  count$ = this.actions$.pipe(
+    ofType<ActionDelayIncrement>(CounterActionTypes.DELAY_INCREMENT),
+    debounce(() => timer(1000)),
+    concatMap(() => {
+      return of(new ActionIncrement());
     })
   );
 }
