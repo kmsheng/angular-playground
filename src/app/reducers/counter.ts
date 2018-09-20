86136ff44280c3112885b2482226ac31871b4e86
kmsheng <kmsh3ng@gmail.com>
2018-09-20 10:47:16 +0800

    feat: update reducer counter

--- src/app/reducers/counter.ts
+++ src/app/reducers/counter.ts
@@ -1,8 +1,33 @@
 import { Action } from '@ngrx/store';
 
-export const INCREMENT = 'INCREMENT';
-export const DECREMENT = 'DECREMENT';
-export const RESET = 'RESET';
+export enum CounterActionTypes {
+  INCREMENT = '[Counter] INCREMENT',
+  DECREMENT = '[Counter] DECREMENT',
+  RESET = '[Counter] RESET',
+  DELAY_INCREMENT = '[Counter] DELAY_INCREMENT'
+}
+
+export class ActionIncrement implements Action {
+  readonly type = CounterActionTypes.INCREMENT;
+  constructor() {}
+}
+
+export class ActionDecrement implements Action {
+  readonly type = CounterActionTypes.DECREMENT;
+  constructor() {}
+}
+
+export class ActionReset implements Action {
+  readonly type = CounterActionTypes.RESET;
+  constructor() {}
+}
+
+export class ActionDelayIncrement implements Action {
+  readonly type = CounterActionTypes.DELAY_INCREMENT,
+  constructor() {}
+}
+
+type Actions = ActionIncrement | ActionDecrement | ActionReset | ActionDelayIncrement;
 
 export interface State {
   count: number;
@@ -10,17 +35,17 @@ export interface State {
 
 const initialState = 0;
 
-export function counterReducer(state: number = initialState, action: Action) {
+export function counterReducer(state: number = initialState, action: Actions) {
 
   switch (action.type) {
 
-    case INCREMENT:
+    case CounterActionTypes.INCREMENT:
       return state + 1;
 
-    case DECREMENT:
+    case CounterActionTypes.DECREMENT:
       return state - 1;
 
-    case RESET:
+    case CounterActionTypes.RESET:
       return 0;
 
     default:
